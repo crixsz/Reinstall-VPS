b5be0958ef4e4708970513b580f4f6e0cb969b59
crixsz <89030463+crixsz@users.noreply.github.com>
2023-04-28 20:09:06 +0800

    fix error in wget package include

--- CoreShell/Core_Install_v5.3.sh
+++ CoreShell/Core_Install_v5.3.sh
@@ -686,7 +686,7 @@ tasksel tasksel/first multiselect minimal
 d-i pkgsel/update-policy select none
 d-i pkgsel/include string openssh-server
 d-i pkgsel/upgrade select none
-d-i pkgsel/include string wget curl
+d-i pkgsel/include string wget
 
 popularity-contest popularity-contest/participate boolean false
 
